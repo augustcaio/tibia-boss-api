ce06a5a2b1915c48c481c59c0a6a9a1b2c415f22
augustcaio <caioaugusto930@gmail.com>
2025-12-30 10:59:41 -0400

    style: fix spacing in test_pydantic_dump.py to satisfy black

--- test_pydantic_dump.py
+++ test_pydantic_dump.py
@@ -3,9 +3,11 @@ from typing import List, Optional
 
 from pydantic import BaseModel, ConfigDict
 
+
 class BossVisuals(BaseModel):
     gif_url: Optional[str] = None
     filename: Optional[str] = None
+
 
 class BossModel(BaseModel):
     name: str
@@ -13,6 +15,7 @@ class BossModel(BaseModel):
     hp: Optional[int] = None
     visuals: Optional[BossVisuals] = None
     model_config = ConfigDict(frozen=False, extra="ignore")
+
 
 # Test model_dump
 boss = BossModel(name="Morgaroth", hp=100000)
